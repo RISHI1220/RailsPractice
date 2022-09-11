338cc4383dcd8a767f2fb425b6c33680ad6ff020
Rishi <Rishibarua08@gmail.com>
2022-09-11 10:35:19 +0530

    CRUD operations complete

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles=Article.all
+  end
+  def show
+    # byebug
+    @article = Article.find(params[:id])
+    # render json: @article
+  end
+  def new
+    @article = Article.new
+  end
+
+  def create
+    byebug
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to show_post_path(@article.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    byebug
+    @article = Article.find(params[:id])
+    # render json: @article
+    if @article.update(article_params)
+      redirect_to show_post_path(@article.id)
+    else
+      # render :edit, status: :unprocessable_entity
+      render json: article
+    end
+  end
+
+  def delete
+    byebug
+    @article=Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  # resources :articles
+  get "articles", to: "articles#index"
+  # get "articles/:id", to: "articles#show", as: :article
+  get "articles/:id", to: "articles#show", as: :show_post
+
+  get "new", to: "articles#new"
+  # post "articles", to: "articles#create"
+  post "new", to: "articles#create", as: :create_post
+
+  get "edit/:id", to: "articles#edit", as: :edit_post
+  patch "edit/:id", to: "articles#update", as: :update_post
+
+  delete "delete/:id", to: "articles#delete", as: :delete_post
+end
